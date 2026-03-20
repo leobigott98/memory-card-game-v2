8938a5d23c291119e0e3289cc1a360ee8e430708
leobigott98 <leonardo.bigott@correo.unimet.edu.ve>
2026-03-19 23:23:23 -0400

    Added capacitorjs config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.doritos.memorygame',
+  appName: 'Doritos Memory Game',
+  webDir: 'dist'
+};
+
+export default config;
